e8694ed322e5bfc42b30c78a9edab11a8d472efd
Savannah Ostrowski <savannahostrowski@gmail.com>
2022-05-08 21:11:12 -0700

    Finished Best Practices for Application Deployment

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,13 +1,21 @@
+import logging
+from multiprocessing import Value
 import sqlite3
+import sys
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+connectionCounter = Value("i", 0)
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    connectionCounter.value += 1
     return connection
 
 # Function to get a post using its ID
@@ -36,22 +44,26 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info('404 - Article does not exist')
       return render_template('404.html'), 404
     else:
+      app.logger.info('Article {} was retrieved.'.format(post['title']))
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('About Us page was accessed.')
     return render_template('about.html')
 
 # Define the post creation functionality 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
+    title=None
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-
+    
         if not title:
             flash('Title is required!')
         else:
@@ -60,10 +72,28 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.info('New article called {} was created'.format(title))
             return redirect(url_for('index'))
-
     return render_template('create.html')
+
+@app.route('/healthz')
+def healthz():
+    response = app.response_class(
+        response = json.dumps({"result": "OK-healthy"}),
+        status=200,
+        mimetype="application/json"
+    )
+    return response
+
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    posts = len(connection.execute('SELECT * FROM posts').fetchall())
+    response = app.response_class(
+        response = json.dumps({"db_connection_count": connectionCounter.value, "post_count": posts}),
+        status=200
+    )
+    return response
 
 # start the application on port 3111
 if __name__ == "__main__":
